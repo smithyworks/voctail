11b30a46b27ad12f0aa2de7e55b10ea6287f435e
ga45cep <benedikt.rank@tum.de>
2020-06-04 12:15:57 +0200

    quizzes empty page

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,8 +1,10 @@
 import DashboardPage from "./DashboardPage.js";
+import QuizzesPage from "./QuizzesPage.js";
 import SigninPage from "./SigninPage.js";
 import SignoutRedirect from "./SignoutRedirect.js";
 import Error404Page from "./Error404Page.js";
 import IntroPage from "./IntroPage.js";
 import AdminPage from "./AdminPage.js";
 
-export { DashboardPage, SigninPage, SignoutRedirect, Error404Page, IntroPage, AdminPage };
+
+export { DashboardPage, QuizzesPage, SigninPage, SignoutRedirect, Error404Page, IntroPage, AdminPage };
