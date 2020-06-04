0d8bb0e1a701c7ca6c8729869044ae3132f0ff7b
clara-lea <clara.buchholz@tum.de>
2020-06-04 14:12:18 +0200

    added overview to browse through documents

--- frontend/src/components/DocumentPage.js
+++ frontend/src/components/DocumentPage.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Typography as T } from "@material-ui/core";
+import { Grid, Typography as T , GridList, GridListTile, ListSubheader, GridListTileBar, IconButton} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import LocalBarIcon from '@material-ui/icons/LocalBar';
 
 import AppPage from "./AppPage.js";
 import { api } from "../utils";
 
+import tileData from './tileData.js';
+
+
+
 const useStyles = makeStyles({
-    container: { height: "100%", width: "100%" },
-    grid: { height: "100%", width: "100%" },
+    container: { height: 200, width: "100%" },
+    grid: { height: 100, width: "100%" },
     userItem: { width: "150px" },
+
+    //gridlist
+    root: { display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', overflow: 'hidden', backgroundColor: 'rgba(255,255,255,0.54)', },
+    gridList: { width: "100%", height: 800, justifyContent: 'space-around'},
+    icon: {color: 'rgba(255,255,255,0.54)'},
 });
 
 function Documents({ ...props }) {
@@ -27,8 +37,30 @@ function Documents({ ...props }) {
     return (
         <AppPage location="documents" id="documents-page">
         <Grid className={classes.grid} container justify="center" alignItems="center" direction="column">
-        <T variant="h4">Welcome to your Document Overview, {user ? user.name : "..."}!</T>
-    </Grid>
+            <T variant="h4">Welcome to your Document Overview, {user ? user.name : "..."}!</T>
+        </Grid>
+
+            <GridList cellHeight={200} cols={3} container justify="center" alignItems="center" className={classes.gridList}>
+                <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
+                    <ListSubheader component="div">Documents</ListSubheader>
+                </GridListTile>
+                {tileData.map((tile) => (
+                    <GridListTile key={tile.img} cols={1}>
+                        <img src={tile.img} alt={tile.title} />
+                        <GridListTileBar
+                            title={tile.title}
+                            subtitle={<span>description: {tile.description}</span>}
+                            actionIcon={
+                                <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
+                                    <LocalBarIcon />
+                                </IconButton>
+                            }
+                        />
+                    </GridListTile>
+                ))}
+            </GridList>
+
+
     </AppPage>
 );
 }
