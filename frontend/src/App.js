68c78b96043c582b71232738e731174e39f3be22
Christopher Smith <silversmith02@gmail.com>
2020-06-05 13:25:43 +0200

    Run prettier.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
-import { DashboardPage, SigninPage, SignoutRedirect, Error404Page, IntroPage, AdminPage, DocumentPage, DocumentMarkup } from "./components";
+import {
+  DashboardPage,
+  SigninPage,
+  SignoutRedirect,
+  Error404Page,
+  IntroPage,
+  AdminPage,
+  DocumentPage,
+  DocumentMarkup,
+} from "./components";
 import { tokens } from "./utils";
 
 function ProtectedRoute({ ...props }) {
--- frontend/src/components/DocumentPage.js
+++ frontend/src/components/DocumentPage.js
@@ -1,87 +1,143 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Typography as T , GridList, GridListTile, ListSubheader, GridListTileBar, IconButton} from "@material-ui/core";
+import {
+  Grid,
+  Typography as T,
+  GridList,
+  GridListTile,
+  ListSubheader,
+  GridListTileBar,
+  IconButton,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import LocalBarIcon from '@material-ui/icons/LocalBar';
+import LocalBarIcon from "@material-ui/icons/LocalBar";
 
 import AppPage from "./AppPage.js";
 import { api } from "../utils";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 //example tile data
-import exampleImage from '../images/exampleimage.png';
-import textblock from '../images/textblock.png';
+import exampleImage from "../images/exampleimage.png";
+import textblock from "../images/textblock.png";
 
 const useStyles = makeStyles({
-    container: { height: 200, width: "100%" },
-    grid: { height: 100, width: "100%" },
-    userItem: { width: "150px" },
+  container: { height: 200, width: "100%" },
+  grid: { height: 100, width: "100%" },
+  userItem: { width: "150px" },
 
-    //gridlist with documents
-    root: { display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', overflow: 'hidden', backgroundColor: 'rgba(255,255,255,0.54)', },
-    gridList: { width: "100%", height: 800, justifyContent: 'space-around'},
-    icon: {color: 'rgba(255,255,255,0.54)'},
+  //gridlist with documents
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: "rgba(255,255,255,0.54)",
+  },
+  gridList: { width: "100%", height: 800, justifyContent: "space-around" },
+  icon: { color: "rgba(255,255,255,0.54)" },
 });
 
-function Documents({ ...props}, location) {
-    const classes = useStyles();
-    const [user, setUser] = useState();
-    //todo integrate flexible link to individual documents instead of const document markup
-    const documentMarkupLinkClass = location ==="document-markup" ? classes.activeLink : classes.link;
+function Documents({ ...props }, location) {
+  const classes = useStyles();
+  const [user, setUser] = useState();
+  //todo integrate flexible link to individual documents instead of const document markup
+  const documentMarkupLinkClass = location === "document-markup" ? classes.activeLink : classes.link;
 
-    //example tile data
-    const tileData = [
-        { img: exampleImage, title: 'ExampleImage', description: 'this is an example for another preview', author: 'Clara is the author'},
-        { img: textblock, title: 'Munich', description: 'Explanation text about the capital of bavaria', author: 'wikipedia'},
-        { img: textblock, title: 'Munich1', description: 'Explanation text about the capital of bavaria', author: 'wikipedia'},
-        { img: textblock, title: 'Munich2', description: 'Explanation text about the capital of bavaria', author: 'wikipedia'},
-        { img: textblock, title: 'Munich3', description: 'Explanation text about the capital of bavaria', author: 'wikipedia'},
-        { img: textblock, title: 'Munich4', description: 'Explanation text about the capital of bavaria', author: 'wikipedia'},
-        { img: textblock, title: 'Munich5', description: 'Explanation text about the capital of bavaria', author: 'wikipedia'},
-        { img: textblock, title: 'Munich6', description: 'Explanation text about the capital of bavaria', author: 'wikipedia'},
-        { img: textblock, title: 'Munich7', description: 'Explanation text about the capital of bavaria', author: 'wikipedia'},
-    ];
+  //example tile data
+  const tileData = [
+    {
+      img: exampleImage,
+      title: "ExampleImage",
+      description: "this is an example for another preview",
+      author: "Clara is the author",
+    },
+    {
+      img: textblock,
+      title: "Munich",
+      description: "Explanation text about the capital of bavaria",
+      author: "wikipedia",
+    },
+    {
+      img: textblock,
+      title: "Munich1",
+      description: "Explanation text about the capital of bavaria",
+      author: "wikipedia",
+    },
+    {
+      img: textblock,
+      title: "Munich2",
+      description: "Explanation text about the capital of bavaria",
+      author: "wikipedia",
+    },
+    {
+      img: textblock,
+      title: "Munich3",
+      description: "Explanation text about the capital of bavaria",
+      author: "wikipedia",
+    },
+    {
+      img: textblock,
+      title: "Munich4",
+      description: "Explanation text about the capital of bavaria",
+      author: "wikipedia",
+    },
+    {
+      img: textblock,
+      title: "Munich5",
+      description: "Explanation text about the capital of bavaria",
+      author: "wikipedia",
+    },
+    {
+      img: textblock,
+      title: "Munich6",
+      description: "Explanation text about the capital of bavaria",
+      author: "wikipedia",
+    },
+    {
+      img: textblock,
+      title: "Munich7",
+      description: "Explanation text about the capital of bavaria",
+      author: "wikipedia",
+    },
+  ];
 
-    useEffect(() => {
-        api
-            .user()
-            .then(res => {
-                if (res) setUser(res.data);
-            })
-            .catch(err => console.log(err));
-    }, []);
+  useEffect(() => {
+    api
+      .user()
+      .then((res) => {
+        if (res) setUser(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-        <AppPage location="documents" id="documents-page">
-        <Grid className={classes.grid} container justify="center" alignItems="center" direction="column">
-            <T variant="h4">Welcome to your Document Overview, {user ? user.name : "..."}!</T>
-        </Grid>
-            <GridList cellHeight={200} cols={3} container justify="center" alignItems="center" className={classes.gridList}>
-                <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
-                    <ListSubheader component="div">Documents</ListSubheader>
-                </GridListTile>
-                {tileData.map((tile) => (
-                    <GridListTile key={tile.img} cols={1}>
-                        <img src={tile.img} alt={tile.title} />
-                        <GridListTileBar
-                            title={
-                                <Link to="/document-markup" className={documentMarkupLinkClass}>
-                                    {tile.title}
-                                </Link>
-                                }
-                            subtitle={<span>description: {tile.description}</span>}
-                            actionIcon={
-                                <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
-                                    <LocalBarIcon />
-                                </IconButton>
-                            }
-                        />
-                    </GridListTile>
-                ))}
-            </GridList>
+  return (
+    <AppPage location="documents" id="documents-page">
+      <Grid className={classes.grid} container justify="center" alignItems="center" direction="column">
+        <T variant="h4">Welcome to your Document Overview, {user ? user.name : "..."}!</T>
+      </Grid>
+      <GridList cellHeight={200} cols={3} container justify="center" alignItems="center" className={classes.gridList}>
+        <GridListTile key="Subheader" cols={3} style={{ height: "auto" }}>
+          <ListSubheader component="div">Documents</ListSubheader>
+        </GridListTile>
+        {tileData.map((tile) => (
+          <GridListTile key={tile.img} cols={1}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={
+                <Link to="/document-markup" className={documentMarkupLinkClass}>
+                  {tile.title}
+                </Link>
+              }
+              subtitle={<span>description: {tile.description}</span>}
+              actionIcon={
+                <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
+                  <LocalBarIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
     </AppPage>
-);
+  );
 }
 export default Documents;
-
-
-
