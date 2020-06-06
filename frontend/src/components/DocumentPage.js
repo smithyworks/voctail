3d12b5486f63af1b4d43b48b06557a6187186404
clara-lea <clara.buchholz@tum.de>
2020-06-06 18:35:20 +0200

    added popup to the document overview

--- frontend/src/components/DocumentPage.js
+++ frontend/src/components/DocumentPage.js
@@ -7,9 +7,15 @@ import {
   ListSubheader,
   GridListTileBar,
   IconButton,
+  Button,
+  Dialog,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, WithStyles, withStyles, createStyles, Theme } from "@material-ui/core/styles";
 import LocalBarIcon from "@material-ui/icons/LocalBar";
+import CloseIcon from "@material-ui/icons/Close"
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
 
 import AppPage from "./AppPage.js";
 import { api } from "../utils";
@@ -36,6 +42,96 @@ const useStyles = makeStyles({
   icon: { color: "rgba(255,255,255,0.54)" },
 });
 
+//popup
+const styles = (theme: Theme) =>
+    createStyles({
+      root: {
+        margin: 0,
+        padding: theme.spacing(2),
+      },
+      closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+      },
+    });
+export interface DialogTitleProps extends WithStyles<typeof styles> {
+  id: string,
+  children: React.ReactNode,
+  onClose: () => void
+}
+const DialogTitle = withStyles(styles)((props: DialogTitleProps) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+      <MuiDialogTitle disableTypography className={classes.root} {...other}>
+        <T variant="h6">{children}</T>
+        {onClose ? (
+            <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+              <CloseIcon />
+            </IconButton>
+        ) : null}
+      </MuiDialogTitle>
+  );
+});
+const DialogContent = withStyles((theme: Theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+const DialogActions = withStyles((theme: Theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+//popup for the document you prefer (get some information about the doc before entering viewmode)
+function DocumentOverviewPopup() {
+  //todo use document overview popup
+  const [open, setOpen] =React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+      <div>
+        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+          Dialog öffnen
+        </Button>
+        <Dialog onClose={handleClose} aria-labelledby="document-overview-popup" open={open}>
+          <DialogTitle id="document-overview-popup" onClose={handleClose}>
+            Document: title
+          </DialogTitle>
+          <DialogContent dividers>
+            <T gutterBottom>
+              Description: description
+            </T>
+            <T gutterBottom>
+              This document is provided to you by author
+            </T>
+          </DialogContent>
+          <DialogActions>
+            //todo onclick go to document
+            <Button autoFocus onClick={handleClose} color="primary" href="/document-markup">
+              Open the document in the markup view mode
+            </Button>
+            <Button autoFocus onClick={handleClose} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+  );
+
+}
+
+//overview (browse through documents, see title, preview and some additional information)
 function Documents({ ...props }, location) {
   const classes = useStyles();
   const [user, setUser] = useState();
@@ -122,16 +218,16 @@ function Documents({ ...props }, location) {
           <GridListTile key={tile.img} cols={1}>
             <img src={tile.img} alt={tile.title} />
             <GridListTileBar
-              title={
-                <Link to="/document-markup" className={documentMarkupLinkClass}>
+              title=
+                //<Link to="/document-markup" className={documentMarkupLinkClass}>
                   {tile.title}
-                </Link>
-              }
+                //</Link>
               subtitle={<span>description: {tile.description}</span>}
               actionIcon={
                 <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
                   <LocalBarIcon />
                 </IconButton>
+
               }
             />
           </GridListTile>
@@ -141,3 +237,4 @@ function Documents({ ...props }, location) {
   );
 }
 export default Documents;
+
