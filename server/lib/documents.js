1eae77580ec99bfe7990b048c6b74ca048fa1498
clara-lea <clara.buchholz@tum.de>
2020-06-22 12:57:07 +0200

    wip access server

--- server/lib/documents.js
+++ server/lib/documents.js
@@ -3,10 +3,11 @@ const { query } = require("./db.js");
 
 async function documentsHandler(req, res) {
   try {
+    const {document_id, masquerading}=req.;
     const {
-      rows: [documents],
-    } = await query("SELECT document_id, title, subtitle, description, embedLink, isPublic, text, image, author FROM documents WHERE document_id = $1", [document_id]);
-    res.status(200).json(..documents);
+      rows: [documentRecord],
+    } = await query("SELECT document_id, title, description, isPublic, content, author FROM documents WHERE document_id = $1", [document_id]);
+    res.status(200).json({...documentRecord, masquerading: !!masquerading});
   } catch (err) {
     log(error);
     res.status(500).send("Something went wrong.");
