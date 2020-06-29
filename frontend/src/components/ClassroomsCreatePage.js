9a2b44fc1e9bdc1e6f2fba1191a91795f433ff95
ryan <ge54juc@mytum.de>
2020-06-29 17:13:55 +0200

    New button style

--- frontend/src/components/ClassroomsCreatePage.js
+++ frontend/src/components/ClassroomsCreatePage.js
@@ -1,8 +1,11 @@
+//This page will be used to test components
+
 import React from "react";
-import { Grid, Typography as T } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import AppPage from "./common/AppPage";
+import styled, { css } from "styled-components";
 
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
@@ -10,78 +13,188 @@ import AccountCircle from "@material-ui/icons/AccountCircle";
 import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles({
-  text: {
+  headuptext: {
     paddingTop: "5%",
     paddingBottom: "5%",
     margin: "auto",
     textAlign: "center",
-    textShadow: "1px 1px",
-  },
-  form: {
-    margin: "auto",
-    textAlign: "center",
-    paddingTop: "2%",
-  },
-  container: { height: "100%", width: "100%" },
-  grid: { height: "100%", width: "100%" },
-  userItem: { width: "150px" },
-  button: {
-    textDecoration: "none",
-    color: "#555",
-    padding: "4px 20px 0 20px",
-    fontSize: "16px",
-    display: "flex",
-    alignItems: "center",
-    width: "30%",
-    height: "25%",
-    borderWidth: "3px",
-    "&:hover": {
-      color: "white",
-      backgroundColor: "rgba(0,0,0,0.3)",
-    },
+    fontStyle: "italic",
   },
 });
+
+function MaterialCardWithContentAndActionButtons(props) {
+  return (
+    <Container {...props}>
+      <CardItem1Style>
+        <HeaderStyle>
+          <LeftImage src={require("../images/logo_green.png")}></LeftImage>
+          <HeaderContent>
+            <TextStyle>Title</TextStyle>
+            <NoteTextStyle>Subhead</NoteTextStyle>
+          </HeaderContent>
+        </HeaderStyle>
+      </CardItem1Style>
+      <CardItemImagePlace src={require("../images/logo_green.png")}></CardItemImagePlace>
+      <Body>
+        <BodyText>BuilderX is a screen design tool which codes React Native for you.</BodyText>
+      </Body>
+      <ActionBody>
+        <ActionButton1>
+          <ButtonOverlay>
+            <ActionText1>ACTION 1</ActionText1>
+          </ButtonOverlay>
+        </ActionButton1>
+        <ActionButton2>
+          <ButtonOverlay>
+            <ActionText2>ACTION 2</ActionText2>
+          </ButtonOverlay>
+        </ActionButton2>
+      </ActionBody>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  border-width: 1px;
+  border-radius: 2px;
+  border-color: #ccc;
+  flex-wrap: nowrap;
+  background-color: #fff;
+  overflow: hidden;
+  flex-direction: column;
+  border-style: solid;
+  box-shadow: -2px 2px 1.5px 0.1px #000;
+`;
+
+const ButtonOverlay = styled.button`
+  display: block;
+  background: none;
+  height: 100%;
+  width: 100%;
+  border: none;
+`;
+const CardItem1Style = styled.div`
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  height: 72px;
+  display: flex;
+`;
+
+const HeaderStyle = styled.div`
+  flex: 1 1 0%;
+  flex-direction: row;
+  align-items: center;
+  display: flex;
+`;
+
+const LeftImage = styled.img`
+  height: 40px;
+  width: 100%;
+  background-color: #ccc;
+  border-radius: 20px;
+`;
+
+const HeaderContent = styled.div`
+  padding-left: 16px;
+  justify-content: center;
+  flex-direction: column;
+  display: flex;
+`;
+
+const TextStyle = styled.span`
+  font-family: Roboto;
+  font-size: 16px;
+  color: #000;
+  line-height: 20px;
+`;
+
+const NoteTextStyle = styled.span`
+  font-family: Roboto;
+  font-size: 14px;
+  color: #000;
+  line-height: 16px;
+  opacity: 0.5;
+`;
+
+const CardItemImagePlace = styled.img`
+  background-color: #ccc;
+  flex: 1 1 0%;
+  min-height: 210px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Body = styled.div`
+  padding: 16px;
+  flex-direction: column;
+  display: flex;
+`;
+
+const BodyText = styled.span`
+  font-family: Arial;
+  line-height: 20px;
+  font-size: 14px;
+  color: #424242;
+`;
+
+const ActionBody = styled.div`
+  padding: 8px;
+  flex-direction: column;
+  display: flex;
+`;
+
+const ActionButton1 = styled.div`
+  padding: 8px;
+  height: 36px;
+  flex-direction: column;
+  display: flex;
+  border: none;
+`;
+
+const ActionText1 = styled.span`
+  font-family: Arial;
+  font-size: 14px;
+  color: #000;
+  opacity: 0.9;
+`;
+
+const ActionButton2 = styled.div`
+  padding: 8px;
+  height: 36px;
+  flex-direction: column;
+  display: flex;
+  border: none;
+`;
+
+const ActionText2 = styled.span`
+  font-family: Arial;
+  font-size: 14px;
+  color: #000;
+  opacity: 0.9;
+`;
+
+function HeadUpText(props) {
+  const styles = useStyles();
+  return <Typography className={styles.headuptext}>{props.text}</Typography>;
+}
 
 function ClassroomsCreatePage({ ...props }) {
   const classes = useStyles();
 
   return (
     <AppPage location="classrooms" id="classrooms-page">
-      <T className={classes.text} variant="h4">
-        Creating a classroom...
-      </T>
-      <div>
-        <div className={classes.form}>
-          <Grid container spacing={1} alignItems="flex-end">
-            <Grid item>
-              <AccountCircle />
-            </Grid>
-            <Grid item>
-              <TextField id="input-with-icon-grid" label="Name of your Classroom*" />
-            </Grid>
-          </Grid>
-        </div>
-        <div className={classes.form}>
-          <Grid container spacing={1} alignItems="flex-end">
-            <Grid item>
-              <AccountCircle />
-            </Grid>
-            <Grid item>
-              <TextField id="input-with-icon-grid" label="Select a topic for your Classroom" />
-            </Grid>
-          </Grid>
-        </div>
-        <div className={classes.form}>
-          <Grid container spacing={2} alignItems="flex-end">
-            <Grid item>
-              <AccountCircle />
-            </Grid>
-            <Grid item>
-              <TextField id="input-with-icon-grid" label="Write a description for your students" />
-            </Grid>
-          </Grid>
-        </div>
-      </div>
+      <HeadUpText text="This page is used for testing." />
+      <MaterialCardWithContentAndActionButtons
+        style={{
+          height: 499,
+          width: 460,
+          borderRadius: 31,
+          overflow: "visible",
+        }}
+      ></MaterialCardWithContentAndActionButtons>
     </AppPage>
   );
 }
