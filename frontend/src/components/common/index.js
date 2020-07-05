d9e6d161973528aa127f10ae7ba1a2959232bb81
Christopher Smith <silversmith02@gmail.com>
2020-07-05 08:37:29 +0200

    Initial stab at common components for dashboard.

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -2,5 +2,6 @@ import AppPage, { toasts } from "./AppPage";
 import VTButton from "./VTButton";
 import ConfirmDialog from "./ConfirmDialog";
 import OkDialog from "./OkDialog";
+import DashboardSection from "./DashboardSection";
 
-export { AppPage, toasts, VTButton, ConfirmDialog, OkDialog };
+export { AppPage, toasts, VTButton, ConfirmDialog, OkDialog, DashboardSection };
