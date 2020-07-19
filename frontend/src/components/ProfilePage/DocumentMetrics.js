6c4265a46743be9996f23225ab555e94b9483dfd
clara-lea <clara.buchholz@tum.de>
2020-07-19 14:07:54 +0200

    sort document metrics by last seen + show only if it was seen

--- frontend/src/components/ProfilePage/DocumentMetrics.js
+++ frontend/src/components/ProfilePage/DocumentMetrics.js
@@ -7,15 +7,17 @@ function DocumentMetrics(userId) {
   const [documents, setDocuments] = useState([]);
 
   useEffect(() => {
-    api
-      .fetchDocuments()
-      .then((res) => {
-        if (res) {
-          setDocuments(res.data.documents);
-        }
-      })
-      .catch((err) => console.log(err));
-  }, []);
+    if (userId.userId) {
+      api
+        .getDocumentMetrics(userId.userId)
+        .then((res) => {
+          if (res) {
+            setDocuments(res.data.documents);
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [userId]);
 
   return (
     <Table size="small" component={Paper} elevation={0}>
@@ -23,15 +25,18 @@ function DocumentMetrics(userId) {
         <DocumentRow header />
       </TableHead>
       <TableBody>
-        {documents.map((doc) => (
-          <DocumentRow
-            id={doc.document_id}
-            title={doc.title}
-            author={doc.author}
-            publisher={doc.publisher_id}
-            user={userId ? userId : undefined}
-          />
-        ))}
+        {documents
+          .filter((d) => d.last_seen)
+          .map((doc) => (
+            <DocumentRow
+              id={doc.document_id}
+              title={doc.title}
+              author={doc.author}
+              publisher={doc.publisher_id}
+              user={userId ? userId : undefined}
+              last_seen={doc.last_seen}
+            />
+          ))}
       </TableBody>
     </Table>
   );
