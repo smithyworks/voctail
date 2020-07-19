01f5b74268aae6aee6a414b2aa32ca3ffd870813
clara-lea <clara.buchholz@tum.de>
2020-07-19 14:19:27 +0200

    show "no documents" if no documents were seen

--- frontend/src/components/ProfilePage/DocumentMetrics.js
+++ frontend/src/components/ProfilePage/DocumentMetrics.js
@@ -2,6 +2,7 @@ import { Paper, Table, TableBody, TableHead } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
 import DocumentRow from "./DocumentRow";
 import { api } from "../../utils";
+import Typography from "@material-ui/core/Typography";
 
 function DocumentMetrics(userId) {
   const [documents, setDocuments] = useState([]);
@@ -20,25 +21,32 @@ function DocumentMetrics(userId) {
   }, [userId]);
 
   return (
-    <Table size="small" component={Paper} elevation={0}>
-      <TableHead>
-        <DocumentRow header />
-      </TableHead>
-      <TableBody>
-        {documents
-          .filter((d) => d.last_seen)
-          .map((doc) => (
-            <DocumentRow
-              id={doc.document_id}
-              title={doc.title}
-              author={doc.author}
-              publisher={doc.publisher_id}
-              user={userId ? userId : undefined}
-              last_seen={doc.last_seen}
-            />
-          ))}
-      </TableBody>
-    </Table>
+    <div>
+      {documents.filter((d) => d.last_seen).length === 0 ? (
+        <Typography> Please read a document to show metrics. </Typography>
+      ) : (
+        <Table size="small" component={Paper} elevation={0}>
+          <TableHead>
+            <DocumentRow header />
+          </TableHead>
+
+          <TableBody>
+            {documents
+              .filter((d) => d.last_seen)
+              .map((doc) => (
+                <DocumentRow
+                  id={doc.document_id}
+                  title={doc.title}
+                  author={doc.author}
+                  publisher={doc.publisher_id}
+                  user={userId ? userId : undefined}
+                  last_seen={doc.last_seen}
+                />
+              ))}
+          </TableBody>
+        </Table>
+      )}
+    </div>
   );
 }
 export default DocumentMetrics;
