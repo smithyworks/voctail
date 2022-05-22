fe580813f3f774bb4e407abc612ce8fae2c9a974
Christopher Smith <silversmith02@gmail.com>
2022-05-22 20:28:02 +0200

    Remove 03.words_and_translations.sql file.

---
+++ database/init/03.words_and_translations.sql
@@ -0,0 +1,6 @@
+-- 1210471 dictionary files.
+-- 360734 translations collected.
+-- 0 unique words referenced.
+
+-- This file was generated using dict.cc's EN-DE vocabulary database.
+-- Contents redacted.
